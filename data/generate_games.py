8333ff396fa3690b68184d191d037946509b8dfa
JOSH ARNOLD <jarnold@ucdavis.edu>
2020-02-22 15:15:59 -0800

    initial commit

---
+++ data/generate_games.py
@@ -0,0 +1,29 @@
+"""
+Requires two folders:
+    - landscape_label/
+    - landscape_target/
+
+It searches landscape _target/ and if it finds the same image name in the landscape_label folder, initializes a game from it.
+"""
+
+import os
+import shutil
+import json
+
+if os.path.exists('games/'): shutil.rmtree('games/')
+os.mkdir('games/')
+
+def make_game(idx, target_image_path, target_label_path):
+    game_path = os.path.join('games/', str(idx)+'/')
+    os.mkdir(game_path)
+    os.mkdir(os.path.join(game_path, 'dialog/'))
+    shutil.copy(target_image_path, os.path.join(game_path, 'target_image.jpg'))
+    shutil.copy(target_label_path, os.path.join(game_path, 'target_label.png'))
+    flags = {'finished':False, 'drawer_connected':False, 'teller_connected':False}
+    with open(os.path.join(game_path, 'flags.json'), 'w') as file: json.dump(flags, file)    
+    
+
+for idx, target_image in enumerate([x for x in os.listdir('landscape_target/') if ".jpg" in x]):
+    target_path = os.path.join('landscape_target/', target_image)
+    label_path = os.path.join('landscape_label/', target_image.replace('jpg', 'png'))
+    if os.path.exists(label_path): make_game(idx, target_path, label_path)
